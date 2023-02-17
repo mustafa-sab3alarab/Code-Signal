01ddfafde7053393ad8fd30b66a76afc2988b02f
mustafa-sab3alarab <mustafa777alarab@gmail.com>
2023-02-17 15:46:38 +0100

    solve matrix elements sum

--- src/main/kotlin/MatrixElementsSum.kt
+++ src/main/kotlin/MatrixElementsSum.kt
@@ -4,31 +4,25 @@ fun main() {
         mutableListOf(0, 5, 0, 0),
         mutableListOf(2, 0, 3, 3)
     )
-    matrix.forEach { row ->
-//        row.forEach { print("$it ") }
-        row.forEach { column ->
 
-        }
-    }
-//    val rows = matrix.size
-//    val columns = matrix[0].size
-//    println("Matrix[R=$rows,C=$columns]")
+    val rows = matrix.size
+    val columns = matrix[0].size
+    println("Matrix[R=$rows,C=$columns]")
 
-//    val l1 = mutableListOf(0, 1, 1, 2)
-//    val l2 = mutableListOf(0, 5, 0, 0)
-//    val l3 = mutableListOf(2, 0, 3, 3)
-//
-////    l1.zip(l2).zip(l3).forEach {
-////        println(it.toString().filter { it != '(' && it != ')' })
-////    }
-//
-//
-//    l1.zip(l2).zip(l3).forEach {
-//        val s: String = it.toString().filter { it != '(' && it != ')' }
-//    }
-
+    println(matrixElementsSum(matrix))
 }
 
 fun matrixElementsSum(matrix: MutableList<MutableList<Int>>): Int {
-    return 0
+    val hauntedColumns = mutableSetOf<Int>()
+    var sum = 0
+    for (row in matrix) {
+        for (j in row.indices) {
+            if (row[j] == 0) {
+                hauntedColumns.add(j)
+            } else if (j !in hauntedColumns) {
+                sum += row[j]
+            }
+        }
+    }
+    return sum
 }
